42498886098520b03ae151440ba2aa1c9e77a085
Edisopn Peña <edisonp23@gmail.com>
2023-03-05 20:06:22 -0500

    commit segundo

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const express = require('express');
 const { getConnection } = require('./db/db-conection-mongo');
 
+
+const app = express()
+const port = 3000;
+
 getConnection();
+
+app.use('/usuaruio', require('./router/usuario'));
+app.use('/estado-equipo', require('./router/estadoEquipo'));
+app.use('/inventario', require('./router/inventario'));
+app.use('/marca', require('./router/marca'));
+app.use('/tipo-equipo', require('./router/tipoEquipo'));
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening ${port}`)
+});
